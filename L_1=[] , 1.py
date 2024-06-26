3956f654a1bb2d3fbf2f69f5b596f4860db0d83d
Quisniaver <nico.vegav@duocuc.cl>
2024-06-26 08:48:39 -0400

    Prueba 3 version 1

---
+++ L_1=[] , 1.py
@@ -0,0 +1,11 @@
+L_1=[]
+L_2=["Viña" , "Valparaiso" , "Quilpue"]
+def Pedido_uwu():
+            cliente=input(" Ingrese Nombre \n")
+            direccion=input(" Ingrese Direccion \n ")
+            sector=input(" Ingrese Sector \n ")
+            print(" Seleccione Peso de cilindro ")
+            cil_5=input(" 5Kg - Ingrese cantidad \n ")
+            cil_15=input(" 15Kg - Ingrese cantidad \n")
+            cil_45=input(" 45Kg - Ingrese cantidad \n")
+            L_1.append([cliente, direccion, sector, ("5kg" ,cil_5 ), ("15kg" ,cil_15 ), ("45kg" ,cil_45)])
